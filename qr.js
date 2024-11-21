51b282b619424416936ba5c4d8a9a736d61b3e40
barganakukuhraditya <barganakukuh@gmail.com>
2024-11-21 14:33:48 +0700

    Update QR code authentication keyword for improved security

--- qr.js
+++ qr.js
@@ -2,7 +2,8 @@ import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/j
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
    
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTE3NjAwNTA/dGV4dD13aDR0NWF1dGgw";
+//wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTk3MTAwNDA/dGV4dD13aDR0NWF1dGgw";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/idbiz.github.io"
 deleteCookie(wauthparam.tokencookiename);
