5bab3cd69aa498a4e342ce662872d7deb7d42a65
barganakukuhraditya <barganakukuh@gmail.com>
2024-11-21 14:13:12 +0700

    Update redirect URL in QR authentication parameters

--- qr.js
+++ qr.js
@@ -4,6 +4,6 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5OTE3NjAwNTA/dGV4dD13aDR0NWF1dGgw";
 wauthparam.tokencookiehourslifetime=18;
-wauthparam.redirect ="/id.biz.id"
+wauthparam.redirect ="/idbiz.github.io"
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
